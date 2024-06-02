1520b04f4c7514d54058623f7958a3f21ec27957
Li Hua <lihua@email.com>
2024-06-02 22:23:16 +0800

    Update

--- lihua_telegram_bot/__init__.py
+++ lihua_telegram_bot/__init__.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler
+from telegram.ext import (
+    AIORateLimiter,
+    Application,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+)
 from telegram.ext.filters import COMMAND, TEXT
 
 from lihua_telegram_bot import config
 from lihua_telegram_bot.i18n import _
 from lihua_telegram_bot.log import log
+from faker import Faker
+
+fake = Faker()
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -17,19 +26,21 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     )
 
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def fake_contact(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # pylint: disable=W0613
-    await update.message.reply_text(_("Help!"))
-
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # pylint: disable=W0613
-    await update.message.reply_text(update.message.text)
+    user = update.effective_user
+    await update.get_bot().send_contact(
+        chat_id=update.effective_chat.id,
+        phone_number="+1 (145)141-9198",
+        first_name=user.first_name,
+        last_name=user.last_name,
+    )
 
 
 def main() -> None:
-    app = Application.builder().token(config.TOKEN).build()
+    app = (
+        Application.builder().token(config.TOKEN).rate_limiter(AIORateLimiter()).build()
+    )
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("help", help_command))
-    app.add_handler(MessageHandler(TEXT & ~COMMAND, echo))
+    app.add_handler(CommandHandler("fake", fake_contact))
     app.run_polling(allowed_updates=Update.ALL_TYPES)
