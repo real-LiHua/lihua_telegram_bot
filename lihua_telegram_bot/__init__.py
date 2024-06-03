bbcff948e725c37ea4590307957853b46c5a7dfa
Li Hua <lihua@email.com>
2024-06-03 16:34:49 +0800

    Update

--- lihua_telegram_bot/__init__.py
+++ lihua_telegram_bot/__init__.py
@@ -12,9 +12,14 @@ from telegram.ext.filters import COMMAND, TEXT
 from lihua_telegram_bot import config
 from lihua_telegram_bot.i18n import _
 from lihua_telegram_bot.log import log
-from faker import Faker
 
-fake = Faker()
+
+async def init(app):
+    await app.bot.set_my_short_description("Connected")
+
+
+async def stop(app):
+    await app.bot.set_my_short_description("Disconnected")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -26,21 +31,14 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     )
 
 
-async def fake_contact(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # pylint: disable=W0613
-    user = update.effective_user
-    await update.get_bot().send_contact(
-        chat_id=update.effective_chat.id,
-        phone_number="+1 (145)141-9198",
-        first_name=user.first_name,
-        last_name=user.last_name,
-    )
-
-
 def main() -> None:
     app = (
-        Application.builder().token(config.TOKEN).rate_limiter(AIORateLimiter()).build()
+        Application.builder()
+        .token(config.TOKEN)
+        .rate_limiter(AIORateLimiter())
+        .post_init(init)
+        .post_stop(stop)
+        .build()
     )
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("fake", fake_contact))
     app.run_polling(allowed_updates=Update.ALL_TYPES)
