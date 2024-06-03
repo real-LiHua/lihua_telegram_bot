6bd3b5e1478cb61af679667c847da00aa27a0008
Li Hua <lihua@email.com>
2024-06-04 00:05:29 +0800

    Update

--- lihua_telegram_bot/config.py
+++ lihua_telegram_bot/config.py
@@ -3,5 +3,5 @@ import socket as __socket
 
 __sock = __socket.socket(__socket.AF_INET, __socket.SOCK_DGRAM)
 __sock.connect(("8.8.8.8", 80))
-IP = __sock.getsockname()[0]
+IP = __os.getenv("domain", __sock.getsockname()[0])
 TOKEN = __os.getenv("lihua_telegram_bot")
